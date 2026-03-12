a5ceca5e1bf9b05ad67c81130a2c549d8b5f0fad
Emile-Jn <linters.tamponner_0m@icloud.com>
2026-03-12 16:49:52 +0100

    Save slurm output to dedicated folder on cluster

--- sync_logs_from_cluster.sh
+++ sync_logs_from_cluster.sh
@@ -3,12 +3,20 @@
 # Exit on error
 set -e
 
-LOCAL_DIR="/Users/emilejohnston/DataspellProjects/Linguistic-Steganalysis-with-LLMs/logs"
+LOCAL_DIR="/Users/emilejohnston/DataspellProjects/Linguistic-Steganalysis-with-LLMs"
 REMOTE="e12229987@cluster.datalab.tuwien.ac.at"
-REMOTE_DIR="Linguistic-Steganalysis-with-LLMs/logs"
+REMOTE_DIR="Linguistic-Steganalysis-with-LLMs"
 
 # Pull all new changes from logs directory in slurm cluster to local device (not the other way around) using rsync
 rsync -avP -e ssh \
   --exclude-from='.rsyncignore' \
+  "$REMOTE:$REMOTE_DIR/logs/" \
+  "$LOCAL_DIR/logs/"
+
+# Sync env
+rsync -avP -e ssh \
+  --include='pyproject.toml' \
+  --include='uv.lock' \
+  --exclude='*' \
   "$REMOTE:$REMOTE_DIR/" \
   "$LOCAL_DIR/"
